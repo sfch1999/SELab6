cd13327210d3190a54d89034de11508e04f64b3b
sfch1999 <sor100100@yahoo.com>
2021-05-13 18:46:36 +0430

    New Calculator

---
+++ src/main/java/calculator/NewCalculator.java
@@ -0,0 +1,25 @@
+package calculator;
+
+public class NewCalculator {
+    private int power(int a, int b) {
+        int res=1;
+        for (int i = 0; i < b; i++) {
+            res*=a;
+        }
+        return res;
+    }
+
+    private int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int calculate(int op1, int op2, char opt) {
+        if (opt == '/') {
+            return divide(op1, op2);
+        }
+        else if(opt == '^'){
+            return power(op1, op2);
+        }
+        return 0;
+    }
+}
